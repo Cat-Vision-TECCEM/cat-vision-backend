b2299c04e3d9d398c960914c90d8705b5d4b231b
David Rodriguez Fragoso <david_under@hotmail.com>
2022-10-20 19:31:08 -0500

    TO DOs
    Se agregaron las rutas necesarias para la funcionalidad basica
    
    Co-Authored-By: Erick Hernández Silva <75198456+EriherSizac@users.noreply.github.com>

--- Global/Routes/Store.py
+++ Global/Routes/Store.py
@@ -24,7 +24,27 @@ estadísticas por tienda
 - los productos
 
 
-estadisticas gobales, estado, ciudad
+estadisticas gobales, estado
 la tiendita que mas compra
 la tiendita que menos compra
+
+-------------------
+
+Ruta que devuelva todas las tienditas 
+- toda la informacion
+
+Ruta que devuelve los pedidos ordenados en orden descendente para 
+
+Ruta para crear pedido 
+
+IMPORTANTE
+
+RUTA PARA ESTADISTICAS DE LA TIENDA
+- los productos de la tienda separados en agotados, existentes
+- los productos 3 productos mas comprados en un intervalo de fechas 
+- grafica de linea con el historico de compras con intervalos de temporalidad de 1 mes
+
+
+BASE DEDATOS
+QUITAR CANTIDAD
 """
