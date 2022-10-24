51248b98dcc175a4497bbbb7c2c1893191f36176
Edurdgzl <edurdgzl@hotmail.com>
2022-10-24 16:48:34 -0500

    test

--- routes/example/example.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-example = Blueprint("example", __name__, url_prefix="/example")
-
-
-@example.route("/", methods=['GET'])
-def test():
-    return "Welcome, example", 200
