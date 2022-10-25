7c494a79133ccf8bc3394dbd0d79da7d010196d9
Liam Garay <65176402+A01750632@users.noreply.github.com>
2022-10-24 19:48:25 -0500

    time sleep and camera function
     time sleep and camera function
    
    Co-Authored-By: Israel Sánchez Miranda <58601226+tamagochi-descompuesto@users.noreply.github.com>
    Co-Authored-By: A01378649 <65170561+a01378649@users.noreply.github.com>

--- ModelsInTheNuc/mainCatvision.py
+++ ModelsInTheNuc/mainCatvision.py
@@ -1,22 +1,32 @@
 from CatVisionDetectron import *
 from ModeloJoe.modeloJoe import *
 import uuid
-
 import cv2
+import time
 
-cap = cv2.VideoCapture(0) 
-ret,frame = cap.read() 
+if __name__ == "__main__":
+    #time.sleep(1800)
+    cap = cv2.VideoCapture(1,cv2.CAP_DSHOW) 
 
-while(True): 
-    if cv2.waitKey(1) & 0xFF == ord('y'):
-        imageName = 'images/'+str(uuid.uuid4())+'.jpg'
-        cv2.imwrite(imageName,frame)
-        cv2.destroyAllWindows()
-        break
+    while(True):
+        ret,frame = cap.read() 
+        cv2.imshow("Result", frame) 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            imageName = 'images/'+str(uuid.uuid4())+'.jpg'
+            cv2.imwrite(imageName,frame)
+            cv2.destroyAllWindows()
+            break
 
-cap.release()
+    cap.release()
 
-predictionDetectron2(imageName)
-jason = prediction()
-yayhijomuerde = bytes(str(jason),"utf-8")
+    predictionDetectron2(imageName)
+    jason = prediction()
+    yayhijomuerde = bytes(str(jason),"utf-8")
+    print(jason)
+
+
+    predictionDetectron2(imageName)
+    jason = prediction()
+    yayhijomuerde = bytes(str(jason),"utf-8")
+
 
