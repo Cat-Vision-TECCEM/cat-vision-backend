ea681e83c8c78d9492c61e2bd89b8dc093058557
Erick Hernandez Silva <hernandezsilvaerick@gmail.com>
2022-11-08 22:17:13 -0600

    company user permissions

--- Global/Controllers/Order.py
+++ Global/Controllers/Order.py
@@ -1,6 +1,6 @@
 from flask import request
 from Global.Classes.Order import Order
-from Global.Utils.sessions import token_valid
+from Global.Utils.sessions import token_valid, company_user_permission
 """
     Script that handles the received requests
     Authors: David Rodriguez Fragoso, Erick Hernandez Silva
@@ -40,6 +40,7 @@ def get_sales_product():
 def get_active_orders():
     try:
         token_valid(request)
+        company_user_permission(request)
         params = {
             'store_id': request.args.get('store_id', None),
             'company_id': request.args.get('company_id', None)
