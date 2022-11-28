bf6ae21b74cb1cf8e9df0f7f741f08c12c9ddfe9
Erick Hernandez Silva <hernandezsilvaerick@gmail.com>
2022-11-27 22:47:45 -0600

    Boto3
    Now you can send an image as form-data in /product/create and the image will be uplaoded to S3 and th elink will be returned and inserted in RDS

--- Global/Utils/s3.py
+++ Global/Utils/s3.py
@@ -1,24 +1,37 @@
 from uuid import uuid4
 import boto3
 import os
+from werkzeug.utils import secure_filename
 
 class S3_Instance():
 
     def __init__(self):
         print(os.environ.get('AWS_ACCESS_KEY_ID'))
         print(os.environ.get('AWS_SECRET_ACCESS_KEY'))
-        self.session = boto3.Session(
+        self.s3 = boto3.client(
+            's3',
             aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key= os.environ.get('AWS_SECRET_ACCESS_KEY')
+            aws_secret_access_key= os.environ.get('AWS_SECRET_ACCESS_KEY'),
+            aws_session_token= os.environ.get('AWS_SESSION_TOKEN')
         )
-        self.s3 = self.session.resource('s3')
 
 
     def upload_image(self, image):
-        object = self.s3.Object(os.environ.get('BUCKET_NAME'), str(uuid4()))
+        image_uuid = str(uuid4())
+        self.s3.upload_fileobj(
+            image,
+            os.environ.get('BUCKET_NAME'),
+            'images/' + image_uuid,
+            ExtraArgs={
+                'ContentType': image.content_type
+            }
 
-        result = object.put(Body=image)
-        secure_filename()
+        )
+        S3_LOCATION = os.environ.get('S3_LOCATION').format(os.environ.get('BUCKET_NAME'))
+        return "{}{}".format(S3_LOCATION, image_uuid)
+        #object = self.s3.Object(os.environ.get('BUCKET_NAME'), 'images/'+str(uuid4()))
+
+        #result = object.put(Body=image)
 
         response = result.get('ResponseMetadata')
 
